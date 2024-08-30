2c15bcda1ba38ef1c7729284b48175c9b1fb0c81
lbran3 <latavia.branson@gmail.com>
2024-08-30 12:03:28 -0400

    beerbottles

--- src/main/java/pg_14/BeerSong.java
+++ src/main/java/pg_14/BeerSong.java
@@ -1,4 +1,31 @@
 package pg_14;
 
+import java.sql.SQLOutput;
+
 public class BeerSong {
+    public static void main(String[] args) {
+
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum - 1;
+            if (beerNum == 1) {
+                word = "bottle";
+            }
+
+
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " of beer on the wall");
+            } else {
+                System.out.println("No more bottles of beer on the wall");
+            } //end of else
+        } // end of while loop
+    }
 }
